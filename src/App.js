cc17f75521febc8d8b80726469f60114e36d0673
Gino <gino.h.rios@gmail.com>
2022-08-29 16:23:43 -0400

    Modify app.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import './main.scss'
 
 import Nav from './components/Nav'
@@ -10,13 +10,13 @@ import Contact from './components/Contact'
 
 export default function App() {
   return (
-    <BrowserRouter>
+    <HashRouter>
       <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path='Projects' element={<Projects />} />
         <Route path='Contact' element={<Contact />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   )
 }
