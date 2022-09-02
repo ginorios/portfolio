ee833f35b04a33576531a7cc7278c56a712d9a56
Gino <gino.h.rios@gmail.com>
2022-09-02 11:55:19 -0400

    Add disclaimer in Projects component

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -12,6 +12,7 @@ export default function Projects() {
       projectTitle: 'Paradise - [Tech: React/JavaScript, CSS/SCSS, HTML]',
       projectDescription: 'Welcome to Paradise! This is a simple travel guide to get you on your way to have a good time in Paradise(Get it?). There are some countries in this site that details the different cultures practiced. Also, there are directions & links to places of interest & restaurants that you may want to check out.'
     }, 
+      
   ]
 
   let proj = projects.map((mProj) => {
@@ -26,6 +27,7 @@ export default function Projects() {
   return (
     <div className='section'>
       {proj}
+
     </div>
 
   )
